5613fd76e831b6b675ae571a9e701ed02d25076c
Olalekan <Olaleksi4231@gmail.com>
2026-04-15 16:54:11 +0100

    fix: railway port issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,7 @@ await database();
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
